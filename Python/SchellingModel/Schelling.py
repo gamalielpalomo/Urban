8e196302bc8947320c32da0ba4f7fbfc02f4a657
Gamaliel <gama.abi_@hotmail.com>
2017-10-30 14:57:43 -0600

    Quasifinished schelling in python

--- Python/SchellingModel/Schelling.py
+++ Python/SchellingModel/Schelling.py
@@ -1,53 +1,64 @@
 import random as rnd
-import pyglet as pg
+from graphics import *
 from Cell import Cell
-class Schelling:
-	def __init__(self, window_width, window_height):
 
-		#Simulation variables
-		self.cellSize = 2
-		self.types = 2
-		self.nSize = 2
+window = GraphWin("Schelling Model",800,800)
 
-		self.columns = int(window_width/self.cellSize)
-		self.rows = int(window_height/self.cellSize)
-		gridSize = self.columns*self.rows
-		self.board = []
-		self.cells = list()
-		freeCells = list()
+window.setBackground("white")
 
-		for row in range(0, self.rows):
-			self.board.append([])
-			for col in range(0, self.columns):
-				cellType = rnd.random()
-				if cellType < 0.5:
-					freeCells.append(Cell(row,col,0,0))
-					self.board[row].append(0)
-				elif cellType >=0.5 and cellType<0.75:
-					self.cells.append(Cell(row,col,1,0.5))
-					self.board[row].append(1)
-				else:
-					self.cells.append(Cell(row,col,2,0.5))
-					self.board[row].append(2)	
+#Simulation variables
+cellSize = 5
+cellTypes = 2
+nSize = 2
 
-	def draw(self):
-		for element in freeCells:
-			################### Here I left
-			square_coords = (element.column*self.cellSize,element.row*self.cellSize,
-							 element.column*self.cellSize,)
-			pg.graphics.draw(4, pg.gl.GL_QUADS,('v2i', square_coords),('c3B', (142,49,49, 142,49,49, 142,49,49,  142,49,49)))
-		for col in range(0, self.columns):
-			for row in range(0, self.rows):
-				if self.board[col][row] != 0:
-					square_coords = (row*self.cellSize, col*self.cellSize,
-									 row*self.cellSize, col*self.cellSize + self.cellSize,
-									 row*self.cellSize + self.cellSize, col*self.cellSize + self.cellSize,
-									 row*self.cellSize + self.cellSize, col*self.cellSize)
-					if self.board[col][row] == 1:
-						pg.graphics.draw(4, pg.gl.GL_QUADS,('v2i', square_coords),('c3B', (142,49,49, 142,49,49, 142,49,49,  142,49,49)))
-					else:
-						pg.graphics.draw(4, pg.gl.GL_QUADS,('v2i', square_coords),('c3B', (53,191,181,53,191,181,53,191,181,53,191,181)))
-					#print square_coords
+columns = int(window.getWidth()/cellSize)
+rows = int(window.getHeight()/cellSize)
+
+
+gridSize = columns*rows
+board = []
+cells = list()
+freeCells = list()
+
+def Schelling():
+	for row in range(0, rows):
+		board.append([])
+		for col in range(0, columns):
+			cellType = rnd.random()
+			if cellType < 0.5:
+				freeCells.append(Cell(row,col,0,0))
+				board[row].append(0)
+			elif cellType >=0.5 and cellType<0.75:
+				cells.append(Cell(row,col,1,0.5))
+				board[row].append(1)
+			else:
+				cells.append(Cell(row,col,2,0.5))
+				board[row].append(2)	
 
 	
-					
+	#time.sleep(0.)
+	draw()
+	window.getMouse()
+
+def draw():
+	for element in freeCells:
+		cell = Rectangle(Point(element.column*cellSize,element.row*cellSize),Point(element.column*cellSize+cellSize,element.row*cellSize+cellSize))
+		cell.setFill("white")
+		cell.draw(window)
+
+		#pg.graphics.draw(4, pg.gl.GL_QUADS,('v2i', square_coords),('c3B', (255,255,255,255,255,255,255,255,255,255,255,255)))
+	"""
+	for col in range(0, columns):
+		for row in range(0, rows):
+			if board[col][row] != 0:
+				square_coords = (row*cellSize, col*cellSize,
+								 row*cellSize, col*cellSize + cellSize,
+								 row*cellSize + cellSize, col*cellSize + cellSize,
+								 row*cellSize + cellSize, col*cellSize)
+				if board[col][row] == 1:
+					pg.graphics.draw(4, pg.gl.GL_QUADS,('v2i', square_coords),('c3B', (142,49,49, 142,49,49, 142,49,49,  142,49,49)))
+				else:
+					pg.graphics.draw(4, pg.gl.GL_QUADS,('v2i', square_coords),('c3B', (53,191,181,53,191,181,53,191,181,53,191,181)))
+				#print square_coords
+	"""
+Schelling()
--- Python/SchellingModel/main.py
+++ Python/SchellingModel/main.py
@@ -1,18 +1,16 @@
-import pyglet as pg
+from graphics import *
 from Schelling import Schelling
 
-class Window(pg.window.Window):
+class Window(GraphWin):
 
-	def __init__(self):
-		super(Window, self).__init__(900,900)
-		pg.gl.glClearColor(255,255,255,1)
-		self.schelling = Schelling(self.get_size()[0],self.get_size()[1])
+	def __init__(self, title, width, height):
+		super(GraphWin, self).__init__(title,width,height)
+		#self.schelling = Schelling(self.get_size()[0],self.get_size()[1])
 
 	def on_draw(self):
-		#self.clear()
+		self.clear()
 		self.schelling.draw()
 		#self.schelling.update()
 
 if __name__ == '__main__':
-	window = Window()
-	pg.app.run()
+	window = Window("Schelling Model", 800, 800)
